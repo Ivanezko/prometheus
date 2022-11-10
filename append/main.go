e8e14e4d803e4c9f000ff2c852b87287a993c6cf
Ivan Eydemiller <ivan@cyolo.io>
2022-11-10 23:05:17 +0100

    !

--- append/main.go
+++ append/main.go
@@ -3,15 +3,24 @@ package main
 func AppendMake() {
 	n := 10000
 	r := make([]int, 1, n+1)
+	// fmt.Printf("%p\n", r)
 	r[0] = 1
-	ad := [100]int{}
+	ad := [10000]int{}
 	r = append(r, ad[:]...)
+	// fmt.Printf("%p\n", r)
 	_ = r
 }
 
 func AppendSimple() {
 	r := []int{1}
+	// fmt.Printf("%p\n", r)
 	ad := [10000]int{}
 	r = append(r, ad[:]...)
+	// fmt.Printf("%p\n", r)
 	_ = r
+}
+
+func main() {
+	AppendMake()
+	AppendSimple()
 }
